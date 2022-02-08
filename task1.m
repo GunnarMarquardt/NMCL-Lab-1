21fa77ac4408aa9d45b1a8354621d3b917a6a108
MrG2333 <george_stoian@protonmail.ch>
2022-02-08 15:52:23 +0100

    Added Code for first part and basic code.

---
+++ task1.m
@@ -0,0 +1,36 @@
+p = [[0;0] [0;1] [1;0] [1;1]];
+t = [0 1 1 0];
+
+net = newff(p,t,[2], {'tansig', 'logsig'},'traingd','','mse',{},{},'')
+
+%net = init(net); [trained_net, status] = train(net, p ,t);
+
+learning_rates = [0.1 2 20];
+number_epochs = [13000 3000 3000]
+
+
+for i=1:length(learning_rates)
+
+    figure;
+    ax = axes; 
+    hold on;
+    grid on;
+    
+    title_lr = ['lr = ' num2str(learning_rates(i))];
+    title(title_lr);
+    
+
+    for j=1:10
+        net.trainParam.lr = learning_rates(i);
+        net.trainParam.epochs = number_epochs(i);
+        net = init(net); [trained_net, status] = train(net, p ,t);
+        
+        plot(ax, status.perf)
+        %plot_xor(trained_net)
+    end
+    %pause
+    
+end
+
+
+
